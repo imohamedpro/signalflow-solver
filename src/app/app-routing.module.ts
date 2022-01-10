502d49034793c6a2abc2825a1e168cb3bb787f1d
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2022-01-10 22:39:36 +0200

    Added info component ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './Components/info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'info', pathMatch:'full'},
+  {path: 'info', component: InfoComponent},
+  {path: '**', redirectTo: 'info'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
