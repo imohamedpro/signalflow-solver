022673dc103c6c7a4947c7c21209fe9d89bf1d5e
Mohamed Magdy <imohamedpro@icloud.com>
2022-04-16 15:31:52 +0200

    🥳

---
+++ src/main/java/edu/control/signalflow/SignalFlowApplication.java
@@ -0,0 +1,13 @@
+package edu.control.signalflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/control/signalflow/SignalFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.control.signalflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
