307ee160142c6f700239d963b6eaa9488c01b5b6
Mohamed Magdy <imohamedpro@icloud.com>
2022-04-16 16:09:09 +0200

    added controller

---
+++ src/main/java/edu/control/signalflow/Controller/Controller.java
@@ -0,0 +1,48 @@
+package edu.control.signalflow.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin()
+public class Controller {
+
+    /**
+     * Takes a node data from front end
+     * @param node node to be added
+     * @return id for new node
+     */
+    @PutMapping("/node/add")
+    public int addNode(@RequestBody Object node){
+        return 0;
+    }
+
+    /**
+     * Takes a path data from front end
+     * @param path
+     * @return id for new path
+     */
+    @PutMapping("/path/add")
+    public int addPath(@RequestBody Object path){
+        return 0;
+    }
+
+    @DeleteMapping("/node/delete")
+    public void deleteNode(@RequestParam int id){
+        //delete node with id
+    }
+
+    @DeleteMapping("/path/delete")
+    public void deletePath(@RequestParam int id){
+        //delete path with id
+    }
+
+    @PostMapping("/data")
+    public void data(@RequestBody Object data){
+        //table data
+    }
+
+    @GetMapping("/result")
+    public Object result(){
+        return null;
+    }
+}
